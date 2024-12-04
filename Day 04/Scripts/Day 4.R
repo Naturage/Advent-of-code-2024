907b6cacd3bd24e2fa1c9be3a1be050853dacd61
Ignas U <ignas.urbonavicius@kantar.com>
2024-12-04 10:05:05 +0000

    Day 4

---
+++ Day 04/Scripts/Day 4.R
@@ -0,0 +1,70 @@
+source(here::here("common functions.R"))
+
+day <- 4
+
+# Data input
+input <- get_input(day)
+
+input_cleaner <- input %>% 
+  str_split("") %>% 
+  unlist %>% 
+  matrix(nrow = length(input)) %>%
+# pad 3 chars from every side
+  cbind(matrix(rep(".", nrow(.) * 3), ncol = 3),
+        .,
+        matrix(rep(".", nrow(.) * 3), ncol = 3)) %>%
+  rbind(matrix(rep(".", ncol(.) * 3), nrow = 3),
+        .,
+        matrix(rep(".", ncol(.) * 3), nrow = 3))
+
+starting_pts <- which(input_cleaner != ".", arr.ind = T)
+
+# Pt1
+
+check_if_xmas <- function(x, y, dir_x, dir_y, grid = input_cleaner){
+  all(grid[x + 0 * dir_x, y + 0 * dir_y] == "X",
+      grid[x + 1 * dir_x, y + 1 * dir_y] == "M",
+      grid[x + 2 * dir_x, y + 2 * dir_y] == "A",
+      grid[x + 3 * dir_x, y + 3 * dir_y] == "S")
+}
+
+check_all_xmas <- function(x,y){
+  sum(check_if_xmas(x,y, 0, 1),
+      check_if_xmas(x,y, 0,-1),
+      check_if_xmas(x,y, 1, 0),
+      check_if_xmas(x,y,-1, 0),
+      check_if_xmas(x,y, 1, 1),
+      check_if_xmas(x,y, 1,-1),
+      check_if_xmas(x,y,-1, 1),
+      check_if_xmas(x,y,-1,-1))
+}
+
+(ans_1 <- mapply(check_all_xmas,
+                x = starting_pts[,1],
+                y = starting_pts[,2]) %>%
+  sum)
+
+# Pt 2
+
+# dir_x, dir_y here aren't cardinal directions but diagonals
+check_if_crossmas <- function(x, y, dir_x, dir_y, grid = input_cleaner){
+  all(grid[x, y] == "A",
+      
+      grid[x + 1 * dir_x, y + 1 * dir_x] == "M",
+      grid[x - 1 * dir_x, y - 1 * dir_x] == "S",
+      
+      grid[x - 1 * dir_y, y + 1 * dir_y] == "M",
+      grid[x + 1 * dir_y, y - 1 * dir_y] == "S")
+}
+
+check_all_crossmas <- function(x,y){
+  max(check_if_crossmas(x,y, 1, 1),
+      check_if_crossmas(x,y, 1,-1),
+      check_if_crossmas(x,y,-1, 1),
+      check_if_crossmas(x,y,-1,-1))
+}
+
+(ans_2 <- mapply(check_all_crossmas,
+                 x = starting_pts[,1],
+                 y = starting_pts[,2]) %>%
+    sum)
