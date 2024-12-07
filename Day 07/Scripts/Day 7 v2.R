67fa2b75e8ed90de2f3da022acb5c29fc305ca38
Naturage <54602845+Naturage@users.noreply.github.com>
2024-12-07 19:48:39 +0000

    Further Day 7 updates

--- Day 07/Scripts/Day 7 v2.R
+++ Day 07/Scripts/Day 7 v2.R
@@ -11,21 +11,28 @@ equations <- lapply(input_clean, \(x){x[-1]})
 
 can_be_obtained <- function(result, inputs, part){
   
+  last <- length(inputs)
+  digits_last <- floor(log10(inputs[last])) + 1
+  
   # various terminating conditions
-  if (length(inputs) == 1){
+  if (last == 1){
     return(inputs == result)
   }
-  if (inputs[1] > result){
+  if (inputs[last] > result){
     return(FALSE)
   }
   
-  sum_option    <- inputs[1] + inputs[2]
-  mult_option   <- inputs[1] * inputs[2]
-  concat_option <- inputs[1] * 10^(floor(log10(inputs[2])) + 1) + inputs[2]
-  
-  sum_works    <-     can_be_obtained(result, c(sum_option   , inputs[-1:-2]), part)
-  mult_works   <-     can_be_obtained(result, c(mult_option  , inputs[-1:-2]), part)
-  concat_works <- all(can_be_obtained(result, c(concat_option, inputs[-1:-2]), part),part == 2)
+  sum_works    <-     can_be_obtained(result - inputs[last] , inputs[-last], part)
+  if (result %% inputs[last] == 0){
+    mult_works   <-     can_be_obtained(result / inputs[last], inputs[-last], part)
+  } else {
+    mult_works <- FALSE
+  }
+  if ((result %% 10^digits_last == inputs[last]) & (part == 2)){
+    concat_works <- can_be_obtained(result %/% 10^digits_last, inputs[-last], part)
+  } else {
+    concat_works <- FALSE
+  }
   
   return(any(mult_works,sum_works,concat_works))
   
